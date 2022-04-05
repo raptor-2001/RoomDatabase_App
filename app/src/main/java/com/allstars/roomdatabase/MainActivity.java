6c928743b4ec3c02aaad716bcaaca0410d068335
Pratham Yadav <pratham007yadav@gmail.com>
2022-04-05 19:05:14 +0530

    completed

--- app/src/main/java/com/allstars/roomdatabase/MainActivity.java
+++ app/src/main/java/com/allstars/roomdatabase/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
     EditText t1,t2,idNo;
     Button b1,b2,viewSingle,deleteSingle,updateSingle;
     TextView singleView;
-
+    database db;
+    UserDao userDao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,101 +37,115 @@ public class MainActivity extends AppCompatActivity {
         deleteSingle = findViewById(R.id.deleteSingle);
         updateSingle = findViewById(R.id.updateSingle);
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                database db = Room.databaseBuilder(getApplicationContext(),
-                        database.class, "room_db").allowMainThreadQueries().build();
-                UserDao userDao = db.userDao();
+        db = Room.databaseBuilder(getApplicationContext(),
+                database.class, "room_db").allowMainThreadQueries().build();
+        userDao = db.userDao();
 
-                userDao.insert(new User(t1.getText().toString(),t2.getText().toString()));
-                t1.setText("");
-                t2.setText("");
+        b1.setOnClickListener(view -> {
+            if(isValid()) {
+                userDao.insert(new User(t1.getText().toString(), t2.getText().toString()));
+                Toast.makeText(MainActivity.this, "Saved Successfully", Toast.LENGTH_SHORT).show();
             }
+            t1.setText("");
+            t2.setText("");
         });
 
-        b2.setOnClickListener(new View.OnClickListener() {
+        b2.setOnClickListener(view -> startActivity(new Intent(getApplicationContext(),fetchData.class)));
 
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),fetchData.class));
+        viewSingle.setOnClickListener(view -> {
+            if(!isValidId()){
+                return;
             }
-//            @Override
-//            public void onClick(View view) {
-//                database db = Room.databaseBuilder(getApplicationContext(),
-//                        database.class, "room_db").allowMainThreadQueries().build();
-//                UserDao userDao = db.userDao();
-//
-//                List<User> users = userDao.getallusers();
-//                String str= "";
-//
-//                for(User user: users){
-//                    str=str+"\t"+user.firstName+" "+user.lastName+"\n\n";
-//                    data.setText(str);
-//                }
-//            }
-        });
+            int id = Integer.parseInt(String.valueOf(idNo.getText()));
+            List<User> users = userDao.getallusers();
+            String str= "";
 
-        viewSingle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                database db = Room.databaseBuilder(getApplicationContext(),
-                        database.class, "room_db").allowMainThreadQueries().build();
-                UserDao userDao = db.userDao();
-                int id = Integer.parseInt(String.valueOf(idNo.getText()));
-                List<User> users = userDao.getallusers();
-                String str= "";
-
-                for(User user: users){
-                    if(id == user.getUid()){
-                        singleView.setText("id: " + id + "\nuser : "+user.firstName+"\npassword : "+user.lastName+"\n\n");
-                    }
-//                    str=str+"\t"+user.firstName+" "+user.lastName+"\n\n";
-//                    data.setText(str);
-                }
-            }
-        });
-
-        deleteSingle.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                database db = Room.databaseBuilder(getApplicationContext(),
-                        database.class, "room_db").allowMainThreadQueries().build();
-                UserDao userDao = db.userDao();
-
-                int id = Integer.parseInt(String.valueOf(idNo.getText()));
-                List<User> users = userDao.getallusers();
-
-                for(User user: users){
-                    if(id == user.getUid()){
-                        userDao.deleteById(user.getUid());
-                        Toast.makeText(MainActivity.this, "Deleted Successfully", Toast.LENGTH_SHORT).show();
-                        idNo.setText(" ");
-//                        users.remove();
-//
-//                        notifyDataSetChanged();
-                    }
-//                    str=str+"\t"+user.firstName+" "+user.lastName+"\n\n";
-//                    data.setText(str);
+            for(User user: users){
+                if(id == user.getUid()){
+                    singleView.setText("id: " + id + "\nuser : "+user.firstName+"\npassword : "+user.lastName+"\n\n");
                 }
 
             }
+        });
+
+        deleteSingle.setOnClickListener(view -> {
+
+            if(!isValidId()){
+                return;
+            }
+            int id = Integer.parseInt(String.valueOf(idNo.getText()));
+            List<User> users = userDao.getallusers();
+
+            for(User user: users){
+                if(id == user.getUid()){
+
+                    userDao.deleteById(user.getUid());
+                    Toast.makeText(MainActivity.this, "Deleted Successfully", Toast.LENGTH_SHORT).show();
+                    idNo.setText(" ");
+                    return;
+
+                }
+
+            }
+
+            Toast.makeText(MainActivity.this, "Invalid ID", Toast.LENGTH_SHORT).show();
+
+        });
+
+        updateSingle.setOnClickListener(view -> {
+
+
+            if(!isValidId()){
+                return;
+            }
+            int id = Integer.parseInt(String.valueOf(idNo.getText()));
+            List<User> users = userDao.getallusers();
+
+            for(User user: users){
+                if(id == user.getUid()){
+
+
+                     if(isValid()){
+
+                     }else{
+                         userDao.update(t1.getText().toString(),t2.getText().toString(),id);
+                         t1.setText("");
+                         t2.setText("");
+                         Toast.makeText(MainActivity.this, "updated Successfully", Toast.LENGTH_SHORT).show();
+                         return;
+                     }
+
+
+                }
+                Toast.makeText(MainActivity.this, "ID doesn't exists", Toast.LENGTH_SHORT).show();
+
+            }
+
         });
     }
 
+    private boolean isValid() {
+        if(t1.getText().toString().isEmpty() || t2.getText().toString().isEmpty()){
+
+            if(t1.getText().toString().isEmpty()){
+                t1.setError("Required");
+
+            }if(t2.getText().toString().isEmpty()){
+                t2.setError("Required");
+                return false;
+            }
 
 
-//    class bgThread extends Thread{
-//        public void run(){
-//            super.run();
-//            database db = Room.databaseBuilder(getApplicationContext(),
-//                    database.class, "room_db").build();
-//            UserDao userDao = db.userDao();
-//            userDao.insert(new User(t1.getText().toString(),t2.getText().toString()));
-//            t1.setText("");
-//            t2.setText("");
-//
-//        }
-//    }
+        }return true;
+
+    }
+
+    private boolean isValidId(){
+        if(idNo.getText().toString().isEmpty()){
+            idNo.setError("Required");
+            return false;
+        }
+        return true;
+    }
 
 }
--- app/src/main/java/com/allstars/roomdatabase/UserDao.java
+++ app/src/main/java/com/allstars/roomdatabase/UserDao.java
@@ -5,20 +5,13 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
+import androidx.room.Update;
 
 import java.util.List;
 
 @Dao
 public interface UserDao {
-    @Query("SELECT * FROM user")
-    List<User> getAll();
 
-    @Query("SELECT * FROM user WHERE uid IN (:userIds)")
-    List<User> loadAllByIds(int[] userIds);
-
-    @Query("SELECT * FROM user WHERE first_name LIKE :first AND " +
-            "last_name LIKE :last LIMIT 1")
-    User findByName(String first, String last);
 
 
     @Query("SELECT * FROM user")
@@ -27,10 +20,16 @@ public interface UserDao {
     @Query("DELETE FROM User WHERE uid = :uid")
     void deleteById(int uid);
 
+
+
+    @Query("UPDATE User SET first_name = :first, last_name = :last WHERE uid =:uid")
+    void update(String first, String last, int uid);
+
+
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(User... users);
 
-    @Delete
-    void delete(User user);
+
 
 }
